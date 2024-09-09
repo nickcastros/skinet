d54567c3c6e84be5a11f7a66573c5a26dca55672
Nicolas Castro <nicolas.castro@tantto.com.br>
2024-09-09 11:50:44 -0300

    Update login.component.ts

--- skinet-frontend/src/app/features/account/login/login.component.ts
+++ skinet-frontend/src/app/features/account/login/login.component.ts
@@ -1,12 +1,8 @@
 import { Component, inject } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { MatButton } from '@angular/material/button';
-import { MatCard, MatCardModule } from '@angular/material/card';
-import {
-  MatFormField,
-  MatFormFieldModule,
-  MatLabel,
-} from '@angular/material/form-field';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormField, MatLabel } from '@angular/material/form-field';
 import { AccountService } from '../../../core/services/account.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { MatInputModule } from '@angular/material/input';
